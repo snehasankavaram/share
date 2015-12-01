86c97bf335207f205857fde387e704200b9a4779
James Jia <jamesjia@berkeley.edu>
2015-11-30 16:13:12 -0800

    Finished wear connection hookups for accept and decline button

--- Application/wear/src/main/java/com/share/AcceptConnectionActivity.java
+++ Application/wear/src/main/java/com/share/AcceptConnectionActivity.java
@@ -1,13 +1,22 @@
 package com.share;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.wearable.view.WatchViewStub;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.example.james.sharedclasses.Contact;
+import com.example.james.sharedclasses.Profile;
 
 public class AcceptConnectionActivity extends Activity {
 
-    private TextView mTextView;
+    private ImageButton accept;
+    private ImageButton decline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +26,55 @@ public class AcceptConnectionActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                accept = (ImageButton) stub.findViewById(R.id.acceptButton);
+                accept.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        createNotification();
+                        Contact c = new Contact(new Profile("Sally Smith", "CEO"));
+                        Intent i = new Intent(getApplicationContext(), ContactActivity.class);
+                        i.putExtra("contact", c);
+                        startActivity(i);
+
+                    }
+                });
+                decline = (ImageButton) stub.findViewById(R.id.declineButton);
+                decline.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                });
             }
         });
+    }
+
+    private void createNotification() {
+        Contact c = new Contact(new Profile("Sarah Jones", "CEO"));
+        Intent intent = new Intent(getBaseContext(), ContactActivity.class);
+        intent.putExtra("contact", c);
+        PendingIntent openContacts = PendingIntent.getActivity(getBaseContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        int notificationId = 001;
+        // Build intent for notification content
+        Intent viewIntent = new Intent(getBaseContext(), AcceptConnectionActivity.class);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(getBaseContext(), 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(getBaseContext())
+                        .setSmallIcon(R.drawable.logo)
+                        .setContentText("New connection from Sarah Jones")
+                        .setContentIntent(viewPendingIntent)
+                        .addAction(R.drawable.face4,
+                                "Accept contact", openContacts);
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(getBaseContext());
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
     }
 }
